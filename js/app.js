bc8e46d98dea70da1546c1d75c135d1347a73b99
JMMoir <jmmoir@outlook.com>
2019-04-27 22:55:38 +0100

    watch list created, toggle between portfolio and watchlist

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', () =>{
   console.log('javascript loaded');
 
   const form = document.querySelector('#new-stock');
-  const handleFormSubmit = function(event){
+
+  const handleFormSubmit = (event) => {
     event.preventDefault();
     console.log(form);
 
@@ -15,12 +16,17 @@ document.addEventListener('DOMContentLoaded', () =>{
     // Create unordered list element
     const listContainer = document.createElement('div');
     listContainer.setAttribute('id', 'holdings')
-    // Get list reference
+    // Get list references
     const portfolio = document.querySelector('#displayPortfolio');
+    const watchlist = document.querySelector('#displayWatchlist');
 
-    // Add list element to portfolio element
+    // Add list element to portfolio of watchlist element
+    if (portfolio.style.display === "block"){
     portfolio.appendChild(listContainer);
-
+    };
+    if (watchlist.style.display === "block"){
+      watchlist.appendChild(listContainer);
+    }
     // add the form data to portfolio
     const investment = document.createElement('p');
     investment.textContent = `Company:${stock} No. of shares:${amount} Price: ${price}`;
@@ -43,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () =>{
 
   // make it delete stuff
   const deleteAll = document.querySelector('#deleteAllButton');
-  const handleDeleteAll = function(){
+  const handleDeleteAll = () => {
     const portfolio = document.querySelector('#displayPortfolio');
      portfolio.remove()
      location.reload(true)
@@ -52,8 +58,37 @@ document.addEventListener('DOMContentLoaded', () =>{
   // add addEventListener
   deleteAll.addEventListener('click', handleDeleteAll);
 
+  ///////////////////////
 
+  // Radio buttons select which div(portfolio/watchlist) is displayed and added to.
+  //  couldn't get radios to switch divs
 
+  // buttons instead
+  // control portfolio view
+  const portfolioView = document.getElementById('#pButton');
 
+  const handlePortfolioView =function(){
+    console.log(portfolioView);
+    const portfolio = document.querySelector('#displayPortfolio');
+    const watchlist = document.querySelector('#displayWatchlist');
+    portfolio.style.display = 'block';
+    watchlist.style.display = 'none';
+  };
+  // event handler
+  portfolioView.addEventListener('click', handlePortfolioView);
+
+  // control watchlist view
+
+  const watchlistView = document.getElementById('#wButton');
+
+  const handleWatchlistView =function(){
+    console.log(watchlistView);
+    const watchlist = document.querySelector('#displayWatchlist');
+    const portfolio = document.querySelector('#displayPortfolio');
+    watchlist.style.display = 'block';
+    portfolio.style.display = 'none';
+  };
+  // event handler
+  watchlistView.addEventListener('click', handleWatchlistView);
 
 })
